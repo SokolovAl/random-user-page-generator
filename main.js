3afda6047146ab3f7690ccd454383078e0fb17dd
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-07-17 23:20:40 +0300

    add solutions for the ability to save multiple users to local storage and load a saved user from local storage using a list of saved users

--- main.js
+++ main.js
@@ -23,19 +23,36 @@ function updateUserData() {
 }
 
 function saveUserPageData() {
+    const savedUsers = JSON.parse(localStorage.getItem("savedUsers")) || [];
     const userPageData = {
+        id: Date.now(),
         mainUser: apiManager.data.mainUser,
         friends: apiManager.data.friends,
         quote: apiManager.data.quote,
         pokemon: apiManager.data.pokemon,
         meatText: apiManager.data.meatText
     };
-    localStorage.setItem("userPageData", JSON.stringify(userPageData));
+    savedUsers.push(userPageData);
+    localStorage.setItem("savedUsers", JSON.stringify(savedUsers));
     alert("User page saved successfully!");
+    loadSavedUsersList();
 }
 
-function loadUserPageData() {
-    const userPageData = JSON.parse(localStorage.getItem("userPageData"));
+function loadSavedUsersList() {
+    const savedUsers = JSON.parse(localStorage.getItem("savedUsers")) || [];
+    const usersDropdownMenu = document.getElementById("users-dropdown-menu");
+    usersDropdownMenu.innerHTML = "";
+    savedUsers.forEach(user => {
+        const option = document.createElement("option");
+        option.value = user.id;
+        option.innerText = `${user.mainUser.firstName} ${user.mainUser.lastName}`;
+        usersDropdownMenu.appendChild(option);
+    });
+}
+
+function loadUserPageDataById(userId) {
+    const savedUsers = JSON.parse(localStorage.getItem("savedUsers")) || [];
+    const userPageData = savedUsers.find(user => user.id === userId);
     if (userPageData) {
         apiManager.data.mainUser = userPageData.mainUser;
         apiManager.data.friends = userPageData.friends;
@@ -55,6 +72,7 @@ function loadUserPageData() {
     }
 }
 
+loadSavedUsersList();
 updateUserData();
 
 const generateUserButton = document.getElementById("generate-user-btn");
@@ -64,4 +82,8 @@ const saveUserButton = document.getElementById("save-user-btn");
 saveUserButton.addEventListener("click", saveUserPageData);
 
 const loadUserButton = document.getElementById("load-user-btn");
-loadUserButton.addEventListener("click", loadUserPageData);
+loadUserButton.addEventListener("click", () => {
+    const usersDropdownMenu = document.getElementById("users-dropdown-menu");
+    const selectedUserId = usersDropdownMenu.value;
+    loadUserPageDataById(Number(selectedUserId));
+});
